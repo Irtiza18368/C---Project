0dee334855e32bdbd4be5d52ba15e4ecc862605c
Irtiza18368 <Irtiza12550@gmail.com>
2025-07-05 14:45:17 +0100

    Initial Commit

--- Practice Project/Practice.cpp
+++ Practice Project/Practice.cpp
@@ -2,5 +2,41 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World";
+    cout << "Welcome to the Simple Calculator Program" << "\n";
+    cout << "You have the choice to do addition, subtraction, multiplication and division." << "\n";
+    cout << "Press 'q' or 'Q' as the operator to quit." << "\n";
+
+    char op;
+    double n1, n2;
+
+    cout << "Enter operator(+, - , *, / ):" << "\n";
+    cin >> op;
+
+    while (op != 'q' && op != 'Q'){
+        cout << "Enter two numbers: " << "\n";
+        cin >> n1 >> n2;
+ 
+
+        if(op == '+'){
+            cout << n1 << " + " << n2 << " = " << n1 + n2 << "\n";
+        } else if (op == '-'){
+            cout << n1 << " - " << n2 << " = " << n1 - n2 << "\n";
+        } else if (op == '*'){
+            cout << n1 << " * " << n2 << " = " << n1 * n2 << "\n";
+        } else if (op == '/'){
+            if (n2 != 0){
+                cout << n1 << " / " << n2 << " = " << n1 / n2 << "\n";
+            } else {
+                cout << "Error! Division by zero is not allowed" << "\n"; 
+            }
+        } else {
+            cout << "Error! The operator is invalid. Please use a correct operator." << "\n";
+        }
+
+        cout << "Enter operator (+, -, *, /) or q to quit: " << "\n";
+        cin >> op;
+    }
+    
+    cout << "Exiting the calculator." << "\n";
+    return 0;
 }
